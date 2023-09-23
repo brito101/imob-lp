d5ec9b02c6a95337200357dfa918066272bd4635
brito101 <rodrigobrito101@hotmail.com>
2023-09-22 21:34:08 -0300

    fix itens on view

--- resources/views/site/partials/tour.blade.php
+++ resources/views/site/partials/tour.blade.php
@@ -9,7 +9,7 @@
             </div>
             @if ($page->link_tour)
                 <div class="col-md-3 d-flex flex-wrap justify-content-center">
-                    <img src="{{ asset('img/tour.png') }}" alt="pattern" class="pb-2 d-none d-md-block w-100">
+                    <img src="{{ asset('img/tour.png') }}" alt="pattern" class="pb-2 d-none d-md-block w-100" width="306" height="314">
                     <a href="{{ $page->link_tour }}" target="_blank" title="Tour Virtual"
                         class="btn btn-primary cta-button">Faça um tour virtual</a>
                 </div>
