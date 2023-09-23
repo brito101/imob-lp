1a1038ff043ad0d990e01fd34174525f01ef55d8
brito101 <rodrigobrito101@hotmail.com>
2023-09-22 21:33:09 -0300

    fix itens on view

--- resources/views/site/partials/conditions.blade.php
+++ resources/views/site/partials/conditions.blade.php
@@ -2,7 +2,7 @@
     <div class="container py-5">
         <div class="row align-items-center py-5">
             <div class=" col-md-6 ps-md-5">
-                <img src="img/conditions.webp" alt="Consultor" class="img-fluid" id="conditions-img">
+                <img src="img/conditions.webp" alt="Consultor" class="img-fluid" id="conditions-img" width="532" height="335">
             </div>
             <div class="col-md-6 px-4 py-5 text-white">
                 {!! $page->conditions !!}
