a7c6f4b3c208482f188eacf93f2b24a940df2296
brito101 <rodrigobrito101@hotmail.com>
2023-09-22 21:12:13 -0300

    fix itens on view

--- resources/views/site/partials/benefits.blade.php
+++ resources/views/site/partials/benefits.blade.php
@@ -10,10 +10,10 @@
         <div class="row align-items-center py-5">
             <div class=" col-md-6 pe-md-5">
                 @if ($page->benefits_video)
-                    <iframe class="elementor-video" frameborder="0" allowfullscreen="1"
+                    <iframe width="640" height="360" src="{{ $page->benefits_video }}" title="YouTube video player"
+                        frameborder="0" allowfullscreen="1"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        title="O que é felicidade para você? - Felicittà Barra" width="640" height="360"
-                        src="{{ $page->benefits_video }}" id="widget2" data-gtm-yt-inspected-11="true" style="max-width: 100%;"></iframe>
+                        style="max-width: 100%;" data-gtm-yt-inspected-11="true" frameborder="0"></iframe>
                 @endif
             </div>
             <div class="col-md-6 px-4 py-5 pl-md-2 text-white">
