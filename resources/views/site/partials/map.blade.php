4ab20d3b7f4abde018086d919b8b7bf6a641834e
brito101 <rodrigobrito101@hotmail.com>
2023-09-22 21:21:44 -0300

    fix itens on view

--- resources/views/site/partials/map.blade.php
+++ resources/views/site/partials/map.blade.php
@@ -2,6 +2,6 @@
     <div class="container-fluid m-0 p-0">
         <iframe src="{{ $page->map }}" class="col-12" width="600" height="490" style="margin-bottom: -10px;"
             allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" data-gtm-yt-inspected-8="true"
-            frameBorder="0"></iframe>
+            frameBorder="0" title="Localização"></iframe>
     </div>
 </section>
